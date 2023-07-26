93e1ae7bd81383b350d2ee8f250207afede8edae
pseusys <shveitsar215@gmail.com>
2023-07-26 02:55:29 +0200

    sync

---
+++ deploy/follow_the_drow/nodes/drow_detector.py
@@ -0,0 +1,44 @@
+#!/usr/bin python3
+
+from pathlib import Path
+
+from rospy import Publisher, Rate, is_shutdown, spin, loginfo
+from geometry_msgs.msg import Point
+
+import follow_the_drow as ftd
+from follow_the_drow.msg import raw_data
+from follow_the_drow.datasets import DROW_Dataset
+from follow_the_drow.utils.drow_utils import laser_angles
+
+
+class DROWDetector:
+    def __init__(self, dataset: Path) -> None:
+        self.dataset = DROW_Dataset(dataset=dataset)
+        loginfo(f"Dataset includes {len(self.dataset.scan_id)} scans and {len(self.dataset.det_id)} detections")
+        self.raw_data = Publisher(ftd.RAW_DATA_TOPIC, raw_data, queue_size=10)
+
+    def update(self, file_counter, scan_counter):
+        bottom_lidar_points = list()
+        scans = self.dataset.scans[file_counter][scan_counter]
+        angles = laser_angles(scans.shape[-1])
+        bottom_lidar_points = [Point(x=x, y=y) for x, y in zip(scans, angles)]
+        odom_x, odom_y, odom_a = self.dataset.odoms[file_counter][scan_counter]["xya"]
+        odometry_data = Point(x=odom_x, y=odom_y, z=odom_a)
+        self.raw_data.publish(raw_data(bottom_lidar=bottom_lidar_points, odometry=odometry_data))
+
+    def __call__(self) -> None:
+        file_counter, scan_counter = 0, 0
+        rate = Rate(ftd.HEARTBEAT_RATE)
+        while not is_shutdown():
+            if scan_counter >= len(self.dataset.scans[file_counter]):
+                file_counter = file_counter + 1 if file_counter < len(self.dataset.scans) - 1 else 0
+                scan_counter = 0
+            self.update(file_counter, scan_counter)
+            scan_counter += 1
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    ftd.load_args_for_node(ftd.DROW_DETECTOR, ftd)
+    DROWDetector(ftd.DATASET_PATH).__call__()
+    spin()
--- deploy/follow_the_drow/scripts/follow_the_drow/startup.py
+++ deploy/follow_the_drow/scripts/follow_the_drow/startup.py
@@ -8,6 +8,7 @@ LIVE_LOADER = "follow_the_drow_live_loader"
 FILE_LOADER = "follow_the_drow_file_loader"
 VISUALIZER = "follow_the_drow_visualizer"
 ALGORITHMIC_DETECTOR = "follow_the_drow_algorithmic_detector"
+DROW_DETECTOR = "follow_the_drow_drow_detector"
 
 # General arguments
 HEARTBEAT_RATE: int = ...
@@ -16,6 +17,8 @@ RAW_DATA_TOPIC: str = ...
 _RAW_DATA_TOPIC_NAME = "raw_data_topic"
 ALGORITHMIC_DETECTOR_TOPIC: str = ...
 _ALGORITHMIC_DETECTOR_TOPIC_NAME = "algorithmic_detector_topic"
+DROW_DETECTOR_TOPIC: str = ...
+_DROW_DETECTOR_TOPIC_NAME = "drow_detector_topic"
 
 # File loader node arguments
 DATASET_PATH: str = ...
@@ -28,9 +31,12 @@ def load_args_for_node(name: str, this_module):
     HEARTBEAT_RATE = get_param(f"/{_HEARTBEAT_RATE_NAME}")
     RAW_DATA_TOPIC = get_param(f"/{_RAW_DATA_TOPIC_NAME}")
     ALGORITHMIC_DETECTOR_TOPIC = get_param(f"/{_ALGORITHMIC_DETECTOR_TOPIC_NAME}")
+    DROW_DETECTOR_TOPIC = get_param(f"/{_DROW_DETECTOR_TOPIC_NAME}")
 
     if name == FILE_LOADER:
         DATASET_PATH = get_param(f"/{name}/{_DATASET_PATH_NAME}")
+    elif name == DROW_DETECTOR:
+        pass
     else:
         raise RuntimeError(f"Unknown node name '{name}'!")
     reload(this_module)
